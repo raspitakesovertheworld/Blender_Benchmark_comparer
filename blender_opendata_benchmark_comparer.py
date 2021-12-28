f0319913bc9ec111e96d28882b3be73e8a69e9f2
Markus Bawidamann <markustheswiss@gmail.com>
2021-12-27 20:26:15 -0800

    v0.5 Selection for CPU / GPU, plus top 100, top 200 etc. added.

--- blender_opendata_benchmark_comparer.py
+++ blender_opendata_benchmark_comparer.py
@@ -12,11 +12,22 @@ def main():
     if not os.path.exists(extracted_name):
         with zipfile.ZipFile("opendata-latest.zip", 'r') as zip_ref:
             zip_ref.extractall(".")
+    print("Select CPU or GPU:")
+    print("1. CPU")
+    print("2. GPU")
+    input_txt = input("Enter your choice: ")
+    if input_txt == "1":
+        renderdev = "CPU"
+    elif input_txt == "2":
+        renderdev = "GPU"
 
-    print("Select List:")
-    print("1. Top 10 GPU")
-    print("2. Top 50 GPU")
-    print("3. Top 100 GPU")
+    print("   ")
+    print("How many?")
+    print("1. Top 10")
+    print("2. Top 50")
+    print("3. Top 100")
+    print("4. Top 250")
+    print("5. Top 500")
     input_txt = input("Enter your choice: ")
     if input_txt == "1":
         maxcount=10
@@ -24,29 +35,55 @@ def main():
         maxcount = 50
     elif input_txt == "3":
         maxcount = 100
-
+    elif input_txt == "4":
+        maxcount = 250
+    elif input_txt == "5":
+        maxcount = 500
     with jsonlines.open(extracted_name) as reader:
     #data = json.load(json_file)
         totallist=[]
         list_pre=[]
         sublist=[]
         error_count=0
-        for element in reader:
-            #print(json.dumps(element["data"], indent=4, sort_keys=True))
-            #print(json.dumps(element, indent=4, sort_keys=True))
-            #print(element["data"][0]["scene"]["label"])
-            try:
-                if element["data"][0]["scene"]["label"] == "pavillon_barcelona":
-                    if element["data"][0]["device_info"]["device_type"] == "CUDA" or element["data"][0]["device_info"]["device_type"] == "OPTIX" or element["data"][0]["device_info"]["device_type"] == "OPENCL" :
-                        #adding it to the list
-                        sublist.append(element["data"][0]["device_info"]["compute_devices"][0]["name"])
-                        sublist.append(element["data"][0]["device_info"]["device_type"])
-                        sublist.append(element["data"][0]["stats"]["total_render_time"])
-                        list_pre.append(sublist)
-                        sublist=[]
 
-            except KeyError:
-                error_count += 1
+
+        if renderdev == "CPU":
+            # CPU
+            for element in reader:
+            # print(json.dumps(element["data"], indent=4, sort_keys=True))
+            # print(json.dumps(element, indent=4, sort_keys=True))
+            # print(element["data"][0]["scene"]["label"])
+                try:
+                    if element["data"][0]["scene"]["label"] == "pavillon_barcelona":
+                        if element["data"][0]["device_info"]["device_type"] == "CPU":
+                            # adding it to the list
+                            sublist.append(element["data"][0]["device_info"]["compute_devices"][0]["name"])
+                            sublist.append(element["data"][0]["device_info"]["device_type"])
+                            sublist.append(element["data"][0]["stats"]["total_render_time"])
+                            list_pre.append(sublist)
+                            sublist = []
+
+                except KeyError:
+                    error_count += 1
+
+
+
+        elif renderdev == "GPU":
+            #GPU
+            for element in reader:
+
+                try:
+                    if element["data"][0]["scene"]["label"] == "pavillon_barcelona":
+                        if element["data"][0]["device_info"]["device_type"] == "CUDA" or element["data"][0]["device_info"]["device_type"] == "OPTIX" or element["data"][0]["device_info"]["device_type"] == "OPENCL" :
+                            #adding it to the list
+                            sublist.append(element["data"][0]["device_info"]["compute_devices"][0]["name"])
+                            sublist.append(element["data"][0]["device_info"]["device_type"])
+                            sublist.append(element["data"][0]["stats"]["total_render_time"])
+                            list_pre.append(sublist)
+                            sublist=[]
+
+                except KeyError:
+                    error_count += 1
 
     print(error_count)
 
